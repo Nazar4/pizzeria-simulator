5004ac25feac35ee1b9891ef6cb8a1fd3f560d31
Roman <clanvozmezdie@gmail.com>
2023-12-05 20:48:46 +0200

    Fixed build with Pizza Type

--- src/main/java/com/lpnu/PZ/domain/PizzaType.java
+++ src/main/java/com/lpnu/PZ/domain/PizzaType.java
@@ -6,21 +6,24 @@ import lombok.Setter;
 import java.util.Arrays;
 
 public enum PizzaType {
-    MARGHERITA(1),
-    PEPPERONI(1.5),
-    VEGGIE(1.25),
-    SUPREME(1.75),
-    BBQ_CHICKEN(1.8),
-    HAWAIIAN(1.8),
-    MEXICAN(1.8),
-    MUSHROOM_OLIVE(1.5),
-    FOUR_CHEESE(1.25),
-    SPINACH_ARTICHOKE(1.75);
+    MARGHERITA(1, 250),
+    PEPPERONI(1.5, 200),
+    VEGGIE(1.25, 175),
+    SUPREME(1.75, 190),
+    BBQ_CHICKEN(1.5, 230),
+    HAWAIIAN(1.2, 220),
+    MEXICAN(1.8, 210),
+    MUSHROOM_OLIVE(1.5, 270),
+    FOUR_CHEESE(1.25, 255),
+    SPINACH_ARTICHOKE(1.6, 150);
 
     @Getter
     private final double timeComplexity;
+    @Getter
+    private final double price;
 
-    PizzaType(double timeComplexity) {
+    PizzaType(double timeComplexity, double price) {
         this.timeComplexity = timeComplexity;
+        this.price = price;
     }
 }
